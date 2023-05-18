62a2be11dbf5905b657819662bc9dc60eedaeae2
DavideSias <davidesias77@gmail.com>
2023-05-18 13:37:14 +0200

    add migrations table

---
+++ database/migrations/2023_05_18_104443_create_sellers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name', 30);
+            $table->string('slug')->unique();
+            $table->string('vat_number', 11)->unique();
+            $table->string('logo_image')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
---
+++ database/migrations/2023_05_18_104637_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->decimal('price', 7, 2)->unsigned();
+            $table->boolean('visible');
+            $table->smallInteger('quantity');
+            $table->string('color');
+            $table->string('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}

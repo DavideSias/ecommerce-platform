1432f1779a074579586565eb9cbcc5bc5262d530
MicheleRai <rainonemichele1999@gmail.com>
2023-05-19 17:12:52 +0200

    added all controller and nav fixed

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 
-class HomeController extends Controller
+class AdminController extends Controller
 {
     /**
      * Create a new controller instance.

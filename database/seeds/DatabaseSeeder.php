edf5674be522c01def886b5ace3de80e4bd548cc
MicheleRai <rainonemichele1999@gmail.com>
2023-05-22 11:52:23 +0200

    seeder structure added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(AlbumSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(CustomerSeeder::class);
+        $this->call(ItemSeeder::class);
+        $this->call(CategoryItemSeeder::class);
+        $this->call(OrderSeeder::class);
+        $this->call(ItemOrderSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(SellerSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(RoleUserSeeder::class);
+
     }
 }
